155523518502ca6589d0022553b285c13b6b852a
HOZANDUNG <522060647@qq.com>
2021-02-04 14:23:52 +0800

    fetch:重写入库方法,减少代码

--- workflow-service/src/main/java/cn/com/ho/workflow/cmd/SyncProcessCmd.java
+++ workflow-service/src/main/java/cn/com/ho/workflow/cmd/SyncProcessCmd.java
@@ -66,6 +66,7 @@ public class SyncProcessCmd implements Command<Void> {
     @Override
     public Void execute(CommandContext commandContext) {
         ActReModel actReModel = actReModelRepository.findOneByActReModelId(modelId);
+        //  处理全局节点
         this.processGlobal(bpmnModel, actReModel);
 
         int priority = 2;
@@ -75,26 +76,32 @@ public class SyncProcessCmd implements Command<Void> {
             boolean stencil = jsonNode.has("stencil");
             if (stencil) {
                 nodeType = jsonNode.findValue("stencil").findValue("id").asText();
-                if (StringUtils.equals(nodeType, "ExclusiveGateway") ||
-                        StringUtils.equals(nodeType, "UserTask") ||
-                        StringUtils.equals(nodeType, "ManualTask") ||
-                        StringUtils.equals(nodeType, "StartNoneEvent") ||
-                        StringUtils.equals(nodeType, "EndNoneEvent")) {
+                //  属于任务/开始/结束/网关等类型才操作
+                if (StringUtils.equals(nodeType, "ExclusiveGateway") || StringUtils.equals(nodeType, "UserTask") || StringUtils.equals(nodeType, "ManualTask") ||
+                        StringUtils.equals(nodeType, "StartNoneEvent") || StringUtils.equals(nodeType, "EndNoneEvent")) {
+                    //  解析xml获取属性
                     String nodeId = jsonNode.findValue("properties").findValue("overrideid").asText();
                     String nodeName = jsonNode.findValue("properties").findValue("name") == null ? nodeId : jsonNode.findValue("properties").findValue("name").asText();
 
+                    //  解析出来实例化一个节点
                     Node node = new Node();
                     node.setId(nodeId);
                     node.setType(nodeType);
                     node.setName(nodeName);
+
+                    //  任务
                     if (StringUtils.equals(nodeType, "UserTask") || StringUtils.equals(nodeType, "ManualTask")) {
-                        processUserTask(node, bpmnModel, priority++, actReModel);
+                        this.processUserTask(node, bpmnModel, priority++, actReModel);
                     }
+
+                    //  开始
                     if (StringUtils.equals(nodeType, "StartNoneEvent")) {
-                        processStartEvent(node, bpmnModel, priority++, actReModel);
+                        this.processStartEvent(node, bpmnModel, priority++, actReModel);
                     }
+
+                    //  结束
                     if (StringUtils.equals(nodeType, "EndNoneEvent")) {
-                        processEndEvent(node, bpmnModel, priority++, actReModel);
+                        this.processEndEvent(node, bpmnModel, priority++, actReModel);
                     }
                 }
             }
@@ -124,98 +131,26 @@ public class SyncProcessCmd implements Command<Void> {
      */
     private void processGlobal(BpmnModel bpmnModel, ActReModel actReModel) {
         Process process = bpmnModel.getMainProcess();
-        String name = process.getName();
+        BpmConfNode bpmConfNode = this.handleBpmConfNode(process.getId(), "全局", "process", 1, actReModel);
 
-        BpmConfNode bpmConfNode = bpmConfNodeRepository.findOneByCodeAndConfBaseId(process.getId(), actReModel.getId_());
-        LocalDateTime now = LocalDateTime.now();
-        if (bpmConfNode == null) {
-            bpmConfNode = new BpmConfNode();
-
-            bpmConfNode.setId(IdWorker.getFlowIdWorkerInstance().nextIdStr());
-            bpmConfNode.setCode(process.getId());
-            bpmConfNode.setName("全局");
-            bpmConfNode.setType("process");
-            bpmConfNode.setPriority(1);
-            bpmConfNode.setStatus("1");
-            bpmConfNode.setConfBaseId(actReModel.getId_());
-            bpmConfNode.setModelName(name);
-
-            bpmConfNode.setCreateTime(now);
-            bpmConfNode.setUpdateTime(now);
-            bpmConfNode.setCreateBy(Auto);
-            bpmConfNode.setUpdateBy(Auto);
-            bpmConfNodeRepository.insertBpmConfNode(bpmConfNode);
-        } else {
-            bpmConfNode.setStatus("1");
-            bpmConfNode.setCode(process.getId());
-            bpmConfNode.setName("全局");
-            bpmConfNode.setType("process");
-            bpmConfNode.setPriority(1);
-            bpmConfNode.setStatus("1");
-            bpmConfNode.setConfBaseId(actReModel.getId_());
-            bpmConfNode.setModelName(name);
-
-            bpmConfNode.setUpdateTime(now);
-            bpmConfNode.setUpdateBy(Auto);
-            bpmConfNodeRepository.updateBpmConfNode(bpmConfNode);
-        }
-        String bpmConfNodeId = bpmConfNode.getId();
-        bpmConfListenerRepository.deleteByNodeIdAndCreateBy(bpmConfNodeId, Auto);
         // 配置监听器
-        processListener(process.getExecutionListeners(), bpmConfNode);
+        bpmConfListenerRepository.deleteByNodeIdAndCreateBy(bpmConfNode.getId(), Auto);
+        this.processListener(process.getExecutionListeners(), bpmConfNode);
     }
 
     /**
      * 配置用户任务.
      */
     private void processUserTask(Node node, BpmnModel bpmnModel, int priority, ActReModel actReModel) {
-        Process process = bpmnModel.getMainProcess();
-        String name = process.getName();
+        //  先处理图像节点
+        BpmConfNode bpmConfNode = this.handleBpmConfNode(node.getId(), node.getName(), node.getType(), priority, actReModel);
 
-        String actReModelId = actReModel.getId_();
-        String nodeId = node.getId();
-        UserTask userTask = (UserTask) bpmnModel.getFlowElement(nodeId);
-        BpmConfNode bpmConfNode = bpmConfNodeRepository.findOneByCodeAndConfBaseId(nodeId, actReModelId);
-        LocalDateTime now = LocalDateTime.now();
-        if (bpmConfNode == null) {
-            bpmConfNode = new BpmConfNode();
-
-            bpmConfNode.setId(IdWorker.getFlowIdWorkerInstance().nextIdStr());
-            bpmConfNode.setCode(nodeId);
-            bpmConfNode.setName(node.getName());
-            bpmConfNode.setType(node.getType());
-            bpmConfNode.setStatus("1");
-            bpmConfNode.setUseDeptRole("0");
-            bpmConfNode.setPriority(priority);
-            bpmConfNode.setGrabOrder("0");
-            bpmConfNode.setConfBaseId(actReModelId);
-            bpmConfNode.setModelName(name);
-
-            bpmConfNode.setCreateTime(now);
-            bpmConfNode.setUpdateTime(now);
-            bpmConfNode.setCreateBy(Auto);
-            bpmConfNode.setUpdateBy(Auto);
-            bpmConfNodeRepository.insertBpmConfNode(bpmConfNode);
-        } else {
-            bpmConfNode.setCode(nodeId);
-            bpmConfNode.setName(node.getName());
-            bpmConfNode.setType(node.getType());
-            bpmConfNode.setStatus("1");
-            bpmConfNode.setUseDeptRole("0");
-            bpmConfNode.setPriority(priority);
-            bpmConfNode.setGrabOrder("0");
-            bpmConfNode.setConfBaseId(actReModelId);
-            bpmConfNode.setModelName(name);
-
-            bpmConfNode.setUpdateTime(now);
-            bpmConfNode.setUpdateBy(Auto);
-            bpmConfNodeRepository.updateBpmConfNode(bpmConfNode);
-        }
-
+        //  节点强转为用户任务节点,才能用到对应的属性
+        UserTask userTask = (UserTask) bpmnModel.getFlowElement(node.getId());
         String bpmConfNodeId = bpmConfNode.getId();
-        //  每次都先把这个节点对应的user先删掉
+
+        // 配置参与者,以最新为准
         bpmConfUserRepository.deleteByNodeIdAndCreateBy(bpmConfNodeId, Auto);
-        // 配置参与者
         int index = 1;
         index = this.processUserTaskConf(bpmConfNode, userTask.getAssignee(), "0", index);
         for (String candidateUser : userTask.getCandidateUsers()) {
@@ -225,15 +160,13 @@ public class SyncProcessCmd implements Command<Void> {
             this.processUserTaskConf(bpmConfNode, candidateGroup, "2", index);
         }
 
-        //  每次都先把这个节点对应的listener先删掉
+        // 配置监听器,以最新为准
         bpmConfListenerRepository.deleteByNodeIdAndCreateBy(bpmConfNodeId, Auto);
-        // 配置监听器
         this.processListener(userTask.getExecutionListeners(), bpmConfNode);
         this.processListener(userTask.getTaskListeners(), bpmConfNode);
 
-        //  每次都先把这个节点对应的form先删掉
+        // 配置表单,以最新为准
         bpmConfFormRepository.deleteByNodeIdAndCreateBy(bpmConfNodeId, Auto);
-        // 配置表单
         this.processForm(userTask, bpmConfNode);
 
         // 会签只在图做标示,因此没有另外维护选项,无需区分图和额外维护的数据
@@ -301,115 +234,33 @@ public class SyncProcessCmd implements Command<Void> {
      * 配置开始事件.
      */
     private void processStartEvent(Node node, BpmnModel bpmnModel, int priority, ActReModel actReModel) {
-        Process process = bpmnModel.getMainProcess();
-        String name = process.getName();
-
-        String nodeId = node.getId();
-        String actReModelId = actReModel.getId_();
-        BpmConfNode bpmConfNode = bpmConfNodeRepository.findOneByCodeAndConfBaseId(nodeId, actReModelId);
-        LocalDateTime now = LocalDateTime.now();
-        if (bpmConfNode == null) {
-            bpmConfNode = new BpmConfNode();
-
-            bpmConfNode.setId(IdWorker.getFlowIdWorkerInstance().nextIdStr());
-            bpmConfNode.setCode(node.getId());
-            bpmConfNode.setName(node.getName());
-            bpmConfNode.setType(node.getType());
-            bpmConfNode.setPriority(priority);
-            bpmConfNode.setStatus("1");
-            bpmConfNode.setUseDeptRole("0");
-            bpmConfNode.setGrabOrder("0");
-            bpmConfNode.setConfBaseId(actReModelId);
-            bpmConfNode.setModelName(name);
-
-            bpmConfNode.setCreateTime(now);
-            bpmConfNode.setUpdateTime(now);
-            bpmConfNode.setCreateBy(Auto);
-            bpmConfNode.setUpdateBy(Auto);
-            bpmConfNodeRepository.insertBpmConfNode(bpmConfNode);
-        } else {
-            bpmConfNode.setCode(nodeId);
-            bpmConfNode.setName(node.getName());
-            bpmConfNode.setType(node.getType());
-            bpmConfNode.setPriority(priority);
-            bpmConfNode.setStatus("1");
-            bpmConfNode.setUseDeptRole("0");
-            bpmConfNode.setGrabOrder("0");
-            bpmConfNode.setConfBaseId(actReModelId);
-            bpmConfNode.setModelName(name);
-
-            bpmConfNode.setUpdateTime(now);
-            bpmConfNode.setUpdateBy(Auto);
-            bpmConfNodeRepository.updateBpmConfNode(bpmConfNode);
-        }
-
-        FlowElement flowElement = bpmnModel.getFlowElement(node.getId());
-
-        String bpmConfNodeId = bpmConfNode.getId();
-        //  每次都先把这个节点对应的listener先删掉
-        bpmConfListenerRepository.deleteByNodeIdAndCreateBy(bpmConfNodeId, Auto);
-        // 配置监听器
-        this.processListener(flowElement.getExecutionListeners(), bpmConfNode);
-        StartEvent startEvent = (StartEvent) flowElement;
-
-        //  每次都先把这个节点对应的form先删掉
-        bpmConfFormRepository.deleteByNodeIdAndCreateBy(bpmConfNodeId, Auto);
-        // 配置表单
-        this.processForm(startEvent, bpmConfNode);
+        this.handleStartEndEvent(node, bpmnModel, priority, actReModel, true);
     }
 
     /**
      * 配置结束事件.
      */
     private void processEndEvent(Node node, BpmnModel bpmnModel, int priority, ActReModel actReModel) {
-        Process process = bpmnModel.getMainProcess();
-        String name = process.getName();
+        this.handleStartEndEvent(node, bpmnModel, priority, actReModel, false);
+    }
 
-        String nodeId = node.getId();
-        String actReModelId = actReModel.getId_();
-        BpmConfNode bpmConfNode = bpmConfNodeRepository.findOneByCodeAndConfBaseId(nodeId, actReModelId);
-        LocalDateTime now = LocalDateTime.now();
-        if (bpmConfNode == null) {
-            bpmConfNode = new BpmConfNode();
-
-            bpmConfNode.setId(IdWorker.getFlowIdWorkerInstance().nextIdStr());
-            bpmConfNode.setCode(nodeId);
-            bpmConfNode.setName(node.getName());
-            bpmConfNode.setType(node.getType());
-            bpmConfNode.setPriority(priority);
-            bpmConfNode.setStatus("1");
-            bpmConfNode.setUseDeptRole("0");
-            bpmConfNode.setGrabOrder("0");
-            bpmConfNode.setConfBaseId(actReModelId);
-            bpmConfNode.setModelName(name);
-
-            bpmConfNode.setCreateTime(now);
-            bpmConfNode.setUpdateTime(now);
-            bpmConfNode.setCreateBy(Auto);
-            bpmConfNode.setUpdateBy(Auto);
-            bpmConfNodeRepository.insertBpmConfNode(bpmConfNode);
-        } else {
-            bpmConfNode.setCode(nodeId);
-            bpmConfNode.setName(node.getName());
-            bpmConfNode.setType(node.getType());
-            bpmConfNode.setPriority(priority);
-            bpmConfNode.setStatus("1");
-            bpmConfNode.setUseDeptRole("0");
-            bpmConfNode.setGrabOrder("0");
-            bpmConfNode.setConfBaseId(actReModelId);
-            bpmConfNode.setModelName(name);
-
-            bpmConfNode.setUpdateTime(now);
-            bpmConfNode.setUpdateBy(Auto);
-            bpmConfNodeRepository.updateBpmConfNode(bpmConfNode);
-        }
+    private void handleStartEndEvent(Node node, BpmnModel bpmnModel, int priority, ActReModel actReModel, boolean isStartEvent) {
+        BpmConfNode bpmConfNode = handleBpmConfNode(node.getId(), node.getName(), node.getType(), priority, actReModel);
 
         FlowElement flowElement = bpmnModel.getFlowElement(node.getId());
         String bpmConfNodeId = bpmConfNode.getId();
-        //  每次都先把这个节点对应的listener先删掉
+
+        // 配置监听器,以最新为准
         bpmConfListenerRepository.deleteByNodeIdAndCreateBy(bpmConfNodeId, Auto);
-        // 配置监听器
         this.processListener(flowElement.getExecutionListeners(), bpmConfNode);
+
+        //  开始节点配置表单
+        if (isStartEvent) {
+            StartEvent startEvent = (StartEvent) flowElement;
+            // 配置表单,以最新为准
+            bpmConfFormRepository.deleteByNodeIdAndCreateBy(bpmConfNodeId, Auto);
+            this.processForm(startEvent, bpmConfNode);
+        }
     }
 
     /**
@@ -454,27 +305,7 @@ public class SyncProcessCmd implements Command<Void> {
         if (userTask.getFormKey() == null) {
             return;
         }
-
-        String nodeId = bpmConfNode.getId();
-        String nodeCode = bpmConfNode.getCode();
-        String nodeName = bpmConfNode.getName();
-
-        LocalDateTime now = LocalDateTime.now();
-
-        BpmConfForm bpmConfForm = new BpmConfForm();
-        bpmConfForm.setValue(userTask.getFormKey());
-        bpmConfForm.setType("external");//类型 internal:内部 电子表单 external:外部 外部表单
-        bpmConfForm.setStatus("1");
-        bpmConfForm.setNodeId(nodeId);
-        bpmConfForm.setNodeCode(nodeCode);
-        bpmConfForm.setNodeName(nodeName);
-        bpmConfForm.setProcessModelId(this.modelId);
-
-        bpmConfForm.setCreateTime(now);
-        bpmConfForm.setUpdateTime(now);
-        bpmConfForm.setCreateBy(Auto);
-        bpmConfForm.setUpdateBy(Auto);
-        bpmConfFormRepository.insertBpmConfForm(bpmConfForm);
+        this.handleForm(userTask.getFormKey(), bpmConfNode);
     }
 
     /**
@@ -484,6 +315,10 @@ public class SyncProcessCmd implements Command<Void> {
         if (startEvent.getFormKey() == null) {
             return;
         }
+        this.handleForm(startEvent.getFormKey(), bpmConfNode);
+    }
+
+    private void handleForm(String formKey, BpmConfNode bpmConfNode) {
         String nodeId = bpmConfNode.getId();
         String nodeCode = bpmConfNode.getCode();
         String nodeName = bpmConfNode.getName();
@@ -491,9 +326,8 @@ public class SyncProcessCmd implements Command<Void> {
         LocalDateTime now = LocalDateTime.now();
 
         BpmConfForm bpmConfForm = new BpmConfForm();
-
         bpmConfForm.setId(IdWorker.getFlowIdWorkerInstance().nextIdStr());
-        bpmConfForm.setValue(startEvent.getFormKey());
+        bpmConfForm.setValue(formKey);
         bpmConfForm.setType("external");
         bpmConfForm.setStatus("1");
         bpmConfForm.setNodeId(nodeId);
@@ -506,5 +340,43 @@ public class SyncProcessCmd implements Command<Void> {
         bpmConfForm.setCreateBy(Auto);
         bpmConfForm.setUpdateBy(Auto);
         bpmConfFormRepository.insertBpmConfForm(bpmConfForm);
+    }
+
+    private BpmConfNode handleBpmConfNode(String nodeId, String nodeName, String nodeType, int priority, ActReModel actReModel) {
+        String actReModelId = actReModel.getId_();
+        BpmConfNode bpmConfNode = bpmConfNodeRepository.findOneByCodeAndConfBaseId(nodeId, actReModelId);
+        LocalDateTime now = LocalDateTime.now();
+        if (bpmConfNode == null) {
+            bpmConfNode = new BpmConfNode();
+            bpmConfNode.setId(IdWorker.getFlowIdWorkerInstance().nextIdStr());
+            bpmConfNode.setCreateTime(now);
+            bpmConfNode.setUpdateTime(now);
+            bpmConfNode.setCreateBy(Auto);
+            bpmConfNode.setUpdateBy(Auto);
+            bpmConfNodeRepository.insertBpmConfNode(this.buildBpmConfNode(bpmConfNode, nodeId, nodeName, nodeType, bpmnModel, priority, actReModel));
+        } else {
+            bpmConfNode.setUpdateTime(now);
+            bpmConfNode.setUpdateBy(Auto);
+            bpmConfNodeRepository.updateBpmConfNode(this.buildBpmConfNode(bpmConfNode, nodeId, nodeName, nodeType, bpmnModel, priority, actReModel));
+        }
+        return bpmConfNode;
+    }
+
+    private BpmConfNode buildBpmConfNode(BpmConfNode bpmConfNode, String nodeId, String nodeName, String nodeType, BpmnModel bpmnModel, int priority, ActReModel actReModel) {
+        Process process = bpmnModel.getMainProcess();
+        String name = process.getName();
+        String actReModelId = actReModel.getId_();
+
+        bpmConfNode.setCode(nodeId);
+        bpmConfNode.setName(nodeName);
+        bpmConfNode.setType(nodeType);
+        bpmConfNode.setPriority(priority);
+        bpmConfNode.setStatus("1");
+        bpmConfNode.setUserDeptRole("0");
+        bpmConfNode.setGrabOrder("0");
+        bpmConfNode.setConfBaseId(actReModelId);
+        bpmConfNode.setModelName(name);
+
+        return bpmConfNode;
     }
 }
